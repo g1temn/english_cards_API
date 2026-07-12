c4c83cf4f4a05b480ee14845bfca0d40e2346275
Urazovskyi Oleskandr <urazovskyi.sasha@gmail.com>
2026-07-12 20:14:09 +0300

    api endpoints to create new cards and fetch them were added, swagger was set up so that i can authozire before sending requests to authozized endpoints

---
+++ Services/CardService.cs
@@ -0,0 +1,55 @@
+﻿using englishCardsAPI.Data;
+using englishCardsAPI.DTOs;
+using englishCardsAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace englishCardsAPI.Services
+{
+    public class CardService : ICardService
+    {
+        private readonly AppDbContext _context;
+
+        public CardService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<CardResponseDto>> GetCardsByUserIdAsync(int userId)
+        {
+            var cards = await _context.Cards
+                .Where(c => c.UserId == userId)
+                .Select(c => new CardResponseDto
+                {
+                    CardId = c.CardId,
+                    Title = c.Title,
+                    Phoneitcs = c.Phoneitcs,
+                    Meaning = c.Meaning
+                })
+                .ToListAsync();
+
+            return cards;
+        }
+
+        public async Task<CardResponseDto> CreateCardAsync(int userId, CardCreationDto createCardDto)
+        {
+            var newCard = new Card
+            {
+                UserId = userId,
+                Title = createCardDto.Title,
+                Phoneitcs = createCardDto.Phoneitcs,
+                Meaning = createCardDto.Meaning
+            };
+            
+            await _context.Cards.AddAsync(newCard);
+            await _context.SaveChangesAsync();
+
+            return new CardResponseDto
+            {
+                CardId = newCard.CardId,
+                Title = newCard.Title,
+                Phoneitcs = newCard.Phoneitcs,
+                Meaning = newCard.Meaning
+            };
+        }
+    }
+}
