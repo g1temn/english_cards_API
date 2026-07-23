56abcd81fc9d272d05f7813691a7070484c69bf6
Urazovskyi Oleksandr <urazovskyi.sasha@gmail.com>
2026-07-23 16:36:59 +0300

    functionality to build and transfer test questions to the frontend was added

---
+++ DTOs/TestQuestionDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace englishCardsAPI.DTOs
+{
+    public class TestQuestionDto
+    {
+        [Required]
+        public int TestQuestionId { get; set; }
+
+        [Required]
+        public string QuestionedWord { get; set; } = string.Empty;
+        
+        [Required]
+        public IEnumerable<string> Answers { get; set; } = new List<string>();
+        
+        [Required]
+        public string RightAnswer { get; set; } = string.Empty;
+    }
+}
--- Services/TestService.cs
+++ Services/TestService.cs
@@ -49,6 +49,40 @@ namespace englishCardsAPI.Services
                 Score = newTest.Score,
                 TakenAt = newTest.TakenAt
             };
-        }        
+        }
+
+        public async Task<IEnumerable<TestQuestionDto>> GetTestQuestionsAsync(int userId)
+        {
+            var userCards = await _context.Cards
+                .Where(card => card.UserId == userId)
+                .ToListAsync();
+
+            var testQuestions = new List<TestQuestionDto>();
+
+            if(userCards.Count >= 10)
+            {
+                foreach (var card in userCards)
+                {
+                    var answers = userCards
+                        .Where(c => c.CardId != card.CardId)
+                        .OrderBy(c => Random.Shared.Next())
+                        .Take(3)
+                        .Select(c => c.Meaning)
+                        .ToList();
+                    answers.Add(card.Meaning);
+                    answers = answers.OrderBy(a => Random.Shared.Next()).ToList();
+                    testQuestions.Add(new TestQuestionDto
+                    {
+                        TestQuestionId = card.CardId,
+                        QuestionedWord = card.Title,
+                        Answers = answers,
+                        RightAnswer = card.Meaning
+                    });
+                }
+            }
+
+            testQuestions = testQuestions.OrderBy(q => Random.Shared.Next()).Take(10).ToList();
+            return testQuestions;
+        }
     }
 }
